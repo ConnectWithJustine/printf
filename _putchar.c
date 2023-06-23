6a155b468d8de17f0427a805c1f6c507e3228810
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-23 14:58:23 -0700

    add _putchar file

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - a function that writes the character c to stdout
+ * @c: the character to print
+ *
+ * Rturn: on success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
