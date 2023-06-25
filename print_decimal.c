a8bdfb93d32aff32391d11380833618ee5c71177
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:13:06 +0100

    add decimal file

--- print_decimal.c
+++ print_decimal.c
@@ -1,1 +1,22 @@
-#!bin/bash
+#include "main.h"
+
+/**
+ * print_decimal - Prints a decimal (int) number.
+ * @ar: A va_list containing the decimal number to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_decimal(va_list ar)
+{
+    int num = va_arg(ar, int);
+    char buffer[20];
+    int length;
+
+    sprintf(buffer, "%d", num);
+    length = _strlen(buffer);
+
+    for (int i = 0; i < length; i++)
+        _putchar(buffer[i]);
+
+    return (length);
+}
