66b5e2708ea5ce16088b44f4ec3cb373924bbc8d
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-06-25 14:36:17 -0700

    fix error

--- format_handler.c
+++ format_handler.c
@@ -3,58 +3,42 @@
 /**
  * format_handler - Handles the formatting of a single conversion specifier.
  * @specifier: The conversion specifier character.
- * @ar: A va_list containing the arguments.
+ * @args: A va_list containing the arguments.
  *
  * Return: The number of characters printed.
  */
-int format_handler(char specifier, va_list ar)
+int format_handler(char specifier, va_list args)
 {
-	int count = 0;
-
 	switch (specifier)
 	{
 	case 'c':
-		count += print_char(ar);
-		break;
+		return (print_char(args));
 	case 's':
-		count += print_string(ar);
-		break;
+		return (print_string(args));
 	case 'd':
 	case 'i':
-		count += print_decimal(ar);
-		break;
+		return (print_decimal(args));
 	case 'b':
-		count += print_binary(ar);
-		break;
+		return (print_binary(args));
 	case 'u':
-		count += print_unsigned(ar);
-		break;
+		return (print_unsigned(args));
 	case 'o':
-		count += print_octal(ar);
-		break;
+		return (print_octal(args));
 	case 'x':
-		count += print_hexadecimal_lower(ar);
-		break;
+		return (print_hexadecimal_lower(args));
 	case 'X':
-		count += print_hexadecimal_upper(ar);
-		break;
+		return (print_hexadecimal_upper(args));
 	case 'p':
-		count += print_pointer(ar);
-		break;
+		return (print_pointer(args));
 	case 'R':
-		count += print_rot13(ar);
-		break;
+		return (print_rot13(args));
 	case 'S':
-		count += print_string(ar);
-		break;
+		return (print_string(args));
 	case 'r':
-		count += print_reverse(ar);
-		break;
+		return (print_reverse(args));
 	default:
 		_putchar('%');
 		_putchar(specifier);
-		count += 2;
-		break;
+		return (2);
 	}
-	return (count);
 }
