654cc937985f482cc0e49540db7a4520b86b400f
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:12:45 +0100

    add character file

--- print_char.c
+++ print_char.c
@@ -1,19 +1,14 @@
 #include "main.h"
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	char c = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+/**
+ * print_char - Prints a single character.
+ * @ar: A va_list containing the character to be printed.
+ *
+ * Return: Always returns 1 (number of characters printed).
+ */
+int print_char(va_list ar)
+{
+    char c = va_arg(ar, int);
+    _putchar(c);
+    return (1);
 }
-#endif
