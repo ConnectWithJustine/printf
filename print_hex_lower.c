a4d9a01fea917f5048a615a7c5544b607eac655f
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:20:21 +0100

    add lower hexadecimal file

---
+++ print_hex_lower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_hexadecimal_lower - Prints an unsigned decimal number in lowercase hexadecimal format.
+ * @ar: A va_list containing the unsigned decimal number to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_hexadecimal_lower(va_list ar)
+{
+    unsigned int num = va_arg(ar, unsigned int);
+    char buffer[20];
+    int length;
+
+    sprintf(buffer, "%x", num);
+    length = _strlen(buffer);
+
+    for (int i = 0; i < length; i++)
+        _putchar(buffer[i]);
+
+    return (length);
+}
