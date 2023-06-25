3022c9ffadc996a5f2cbd673fa204f56e7f4a5ef
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:15:28 +0100

    add pointer file

--- print_pointer.c
+++ print_pointer.c
@@ -1,52 +1,22 @@
 #include "main.h"
+
 /**
- * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed.
+ * print_pointer - Prints a pointer address.
+ * @ar: A va_list containing the pointer address to be printed.
+ *
+ * Return: The number of characters printed.
  */
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_pointer(va_list ar)
 {
-	char extra_c = 0, padd = ' ';
-	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
-	unsigned long num_addrs;
-	char map_to[] = "0123456789abcdef";
-	void *addrs = va_arg(types, void *);
+    void *ptr = va_arg(ar, void *);
+    char buffer[20];
+    int length;
 
-	UNUSED(width);
-	UNUSED(size);
+    sprintf(buffer, "%p", ptr);
+    length = _strlen(buffer);
 
-	if (addrs == NULL)
-		return (write(1, "(nil)", 5));
+    for (int i = 0; i < length; i++)
+        _putchar(buffer[i]);
 
-	buffer[BUFF_SIZE - 1] = '\0';
-	UNUSED(precision);
-
-	num_addrs = (unsigned long)addrs;
-
-	while (num_addrs > 0)
-	{
-		buffer[ind--] = map_to[num_addrs % 16];
-		num_addrs /= 16;
-		length++;
-	}
-
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
-		padd = '0';
-	if (flags & F_PLUS)
-		extra_c = '+', length++;
-	else if (flags & F_SPACE)
-		extra_c = ' ', length++;
-
-	ind++;
-
-	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
+    return (length);
 }
-#endif
