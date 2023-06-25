705bf31270c87f95f855b659be7336817038f8e2
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:21:09 +0100

    add octal file

---
+++ print_octal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_octal - Prints an unsigned decimal number in octal format.
+ * @ar: A va_list containing the unsigned decimal number to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_octal(va_list ar)
+{
+    unsigned int num = va_arg(ar, unsigned int);
+    char buffer[20];
+    int length;
+
+    sprintf(buffer, "%o", num);
+    length = _strlen(buffer);
+
+    for (int i = 0; i < length; i++)
+        _putchar(buffer[i]);
+
+    return (length);
+}
