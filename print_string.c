6c7dff84af06015186d6ec83fe108e649a259162
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:14:37 +0100

    add string file

--- print_string.c
+++ print_string.c
@@ -1,56 +1,34 @@
 #include "main.h"
+
 /**
- * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_string - Prints a string of characters.
+ * @ar: A va_list containing the string to be printed.
+ *
+ * Return: The number of characters printed.
  */
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_string(va_list ar)
 {
-	int length = 0, i;
-	char *str = va_arg(types, char *);
+    char *str = va_arg(ar, char *);
+    int count = 0;
 
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	if (str == NULL)
-	{
-		str = "(null)";
-		if (precision >= 6)
-			str = "      ";
-	}
+    if (str == NULL)
+        str = "(null)";
 
-	while (str[length] != '\0')
-		length++;
+    for (; *str != '\0'; str++)
+    {
+        if ((*str >= 0 && *str < 32) || *str >= 127)
+        {
+            _putchar('\\');
+            _putchar('x');
+            count += 2;
+            count += print_hex(*str);
+        }
+        else
+        {
+            _putchar(*str);
+            count++;
+        }
+    }
 
-	if (precision >= 0 && precision < length)
-		length = precision;
-
-	if (width > length)
-	{
-		if (flags & F_MINUS)
-		{
-			write(1, &str[0], length);
-			for (i = width - length; i > 0; i--)
-				write(1, " ", 1);
-			return (width);
-		}
-		else
-		{
-			for (i = width - length; i > 0; i--)
-				write(1, " ", 1);
-			write(1, &str[0], length);
-			return (width);
-		}
-	}
-
-	return (write(1, str, length));
+    return (count);
 }
-#endif
