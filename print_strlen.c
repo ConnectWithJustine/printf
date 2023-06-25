d594eba028bc3957059bdaa142cd09cd0328f604
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:14:12 +0100

    add string lenght file

---
+++ print_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string
+ * @str: The string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *str)
+{
+    int length = 0;
+
+    while (str[length] != '\0')
+        length++;
+
+    return (length);
+}
