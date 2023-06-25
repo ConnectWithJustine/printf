5f3d28f5f0b75d6351c2b0f989d409b7d0710df0
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:15:48 +0100

    add percent file

--- print_percent.c
+++ print_percent.c
@@ -1,22 +1,13 @@
 #include "main.h"
+
 /**
- * print_percent - Prints a percent sign
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_percent - Prints a percent character.
+ * @ar: Unused.
+ *
+ * Return: Always returns 1 (number of characters printed).
  */
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_percent(__attribute__((unused)) va_list ar)
 {
-	UNUSED(types);
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	return (write(1, "%%", 1));
-#endif
+    _putchar('%');
+    return (1);
+}
