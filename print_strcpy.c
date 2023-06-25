82db515120d04609a2dbd2fa62e104d754508d5a
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:17:50 +0100

    add string copy file

---
+++ print_strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: The pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+
+    return (dest);
+}
