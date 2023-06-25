6964717ec5d01723ae0ba4b6b18080ee21ac8515
Ugwu Justine <ugwujutineobinna@gmail.com>
2023-06-25 16:20:34 +0100

    add upper hexadecimal file

---
+++ print_hex_upper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_hexadecimal_upper - Prints an unsigned decimal number in uppercase hexadecimal format.
+ * @ar: A va_list containing the unsigned decimal number to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_hexadecimal_upper(va_list ar)
+{
+    unsigned int num = va_arg(ar, unsigned int);
+    char buffer[20];
+    int length;
+
+    sprintf(buffer, "%X", num);
+    length = _strlen(buffer);
+
+    for (int i = 0; i < length; i++)
+        _putchar(buffer[i]);
+
+    return (length);
+}
