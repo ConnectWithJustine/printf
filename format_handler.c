7cd4d2628abb86fb3605fa775b1ef4c120cca106
TinyBila <bilatinym@gmail.com>
2023-06-25 12:03:48 -0700

    update

--- format_handler.c
+++ format_handler.c
@@ -9,53 +9,53 @@
  */
 int format_handler(char specifier, va_list ar)
 {
-    int count = 0;
+	int count = 0;
 
-    switch (specifier)
-    {
-    case 'c':
-        count += print_char(ar);
-        break;
-    case 's':
-        count += print_string(ar);
-        break;
-    case 'd':
-    case 'i':
-        count += print_decimal(ar);
-        break;
-    case 'b':
-        count += print_binary(ar);
-        break;
-    case 'u':
-        count += print_unsigned(ar);
-        break;
-    case 'o':
-        count += print_octal(ar);
-        break;
-    case 'x':
-        count += print_hexadecimal_lower(ar);
-        break;
-    case 'X':
-        count += print_hexadecimal_upper(ar);
-        break;
-    case 'p':
-        count += print_pointer(ar);
-        break;
-    case 'R':
-        count += print_rot13(ar);
-        break;
-    case 'S':
-        count += print_string(ar);
-        break;
-    case 'r':
-        count += print_reverse(ar);
-        break;
-    default:
-        _putchar('%');
-        _putchar(specifier);
-        count += 2;
-        break;
-    }
+	switch (specifier)
+	{
+	case 'c':
+		count += print_char(ar);
+		break;
+	case 's':
+		count += print_string(ar);
+		break;
+	case 'd':
+	case 'i':
+		count += print_decimal(ar);
+		break;
+	case 'b':
+		count += print_binary(ar);
+		break;
+	case 'u':
+		count += print_unsigned(ar);
+		break;
+	case 'o':
+		count += print_octal(ar);
+		break;
+	case 'x':
+		count += print_hexadecimal_lower(ar);
+		break;
+	case 'X':
+		count += print_hexadecimal_upper(ar);
+		break;
+	case 'p':
+		count += print_pointer(ar);
+		break;
+	case 'R':
+		count += print_rot13(ar);
+		break;
+	case 'S':
+        	count += print_string(ar);
+        	break;
+	case 'r':
+        	count += print_reverse(ar);
+        	break;
+	default:
+        	_putchar('%');
+       		 _putchar(specifier);
+        	count += 2;
+        	break;
+	}
 
-    return (count);
+	return (count);
 } 
