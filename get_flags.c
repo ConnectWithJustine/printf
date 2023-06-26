d661b2c8bcdea30a24e652fa7d227b09604b25df
TinyBila <bilatinym@gmail.com>
2023-06-26 14:56:21 -0700

    update

--- get_flags.c
+++ get_flags.c
@@ -13,7 +13,7 @@ int get_flags(const char *format, int *i)
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, FD_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
